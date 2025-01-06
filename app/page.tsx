90b5f1651ea85f655280e2dffe291aeb5f6c16c3
igbafe peter <igbafes8@gmail.com>
2025-01-06 16:44:25 +0100

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,28 @@
-// import Clients from "@/components/Clients";
+'use client';
+
+import dynamic from 'next/dynamic';
+import { navItems } from "@/data";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import Process from "@/components/Process";
 import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-import { navItems } from "@/data";
 
+// Import FloatingNav with no SSR
+const FloatingNav = dynamic(
+  () => import('@/components/ui/FloatingNav').then(mod => mod.FloatingNav),
+  { ssr: false }
+);
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+    <main className="relative bg-black-100 overflow-hidden">
+      <div className="max-w-7xl w-full mx-auto px-5 sm:px-10">
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
-        {/* <Clients /> */}
         <Experience />
         <Process />
         <Footer />
