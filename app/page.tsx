c01036a2ebba00c7a5dfecfdbc4afcfbb0dc78da
igbafe peter <igbafes8@gmail.com>
2025-02-08 16:16:07 +0100

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -8,16 +8,16 @@ import Process from "@/components/Process";
 import RecentProjects from "@/components/RecentProjects";
 
 // Import FloatingNav with no SSR
-const FloatingNav = dynamic(
-  () => import('@/components/ui/FloatingNav').then(mod => mod.FloatingNav),
-  { ssr: false }
-);
+// const FloatingNav = dynamic(
+//   () => import('@/components/ui/FloatingNav').then(mod => mod.FloatingNav),
+//   { ssr: false }
+// );
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 overflow-hidden">
       <div className="max-w-7xl w-full mx-auto px-5 sm:px-10">
-        <FloatingNav navItems={navItems} />
+        {/* <FloatingNav navItems={navItems} /> */}
         <Hero />
         <Grid />
         <RecentProjects />
